aeb8bb449e148fd12a31c139ae4208ccd6996621
MoeinT <moin.torabi@gmail.com>
2023-01-16 11:48:10 +0100

    added kv access policy

--- tf/CommonModules/KeyVault/locals.tf
+++ tf/CommonModules/KeyVault/locals.tf
@@ -1,15 +1,8 @@
 locals {
   DefaultTags = {
-    "Assignment_Code"   = "VY064I"
-    "Business_Owner"    = "bartlomiej.wierzbicki@volvo.com"
     "Environment_Type"  = var.env
-    "ID"                = "109460"
     "InfoSeC"           = "Confidential"
-    "PDS"               = "Medium"
-    "Service_Level"     = "CEP_Basic"
-    "Technical_Contact" = "daniel.garrivier@volvo.com"
-    "UDP_RGTag"         = "AnotherValue"
-    "UDP_ResTag"        = "MyValue"
+    "Technical_Contact" = "moin.torabi@gmail.com"
   }
 }
 
--- tf/TFProjects/tfDatabricks/azure/KeyVault.tf
+++ tf/TFProjects/tfDatabricks/azure/KeyVault.tf
@@ -12,28 +12,23 @@ module "kv" {
   }
 }
 
-# module "kvpolicies" {
-#   source = "../../../CommonModules/KVAccessPolicy"
+module "kvpolicies" {
+  source = "../../../CommonModules/KVAccessPolicy"
 
-#   properties = {
-#     "moein-${var.env}" = {
-#       "key_vault_id"       = module.kv.KVids["tf-01-kv-${var.env}"],
-#       "object_id"          = var.moeinobji,
-#       "KeyPermissions"     = ["Get", "List", "Create"],
-#       "SecretPermissions"  = ["Get", "List", "Set"],
-#       "StoragePermissions" = ["Get", "List"]
-#     },
-#     "sp-${var.env}" = {
-#       "key_vault_id"       = module.kv.KVids["tf-01-kv-${var.env}"],
-#       "object_id"          = var.spobjid,
-#       "KeyPermissions"     = ["Get", "List", "Create"],
-#       "SecretPermissions"  = ["Get", "List", "Set"],
-#       "StoragePermissions" = ["Get", "List"]
-#     },
-#     "adf-${var.env}" = {
-#       "key_vault_id"   = module.kv.KVids["tf-01-kv-${var.env}"],
-#       "object_id"      = module.adfs.principal_id["adf-tfdemo-01-${var.env}"],
-#       "KeyPermissions" = ["Get", "UnwrapKey", "WrapKey"]
-#     }
-#   }
-# }
+  properties = {
+    "moein-${var.env}" = {
+      "key_vault_id"       = module.kv.KVids["kv-tfdb-${var.env}"],
+      "object_id"          = var.moeinobji,
+      "KeyPermissions"     = ["Get", "List", "Create"],
+      "SecretPermissions"  = ["Get", "List", "Set"],
+      "StoragePermissions" = ["Get", "List"]
+    },
+    "sp-${var.env}" = {
+      "key_vault_id"       = module.kv.KVids["kv-tfdb-${var.env}"],
+      "object_id"          = var.spobjid,
+      "KeyPermissions"     = ["Get", "List", "Create"],
+      "SecretPermissions"  = ["Get", "List", "Set"],
+      "StoragePermissions" = ["Get", "List"]
+    }
+  }
+}

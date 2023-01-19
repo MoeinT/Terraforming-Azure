289144e52fd4788d17d38ec631e2aa407c7c5949
MoeinT <moin.torabi@gmail.com>
2023-01-19 19:42:58 +0100

    added a adf and a kv ls

--- tf/TFProjects/tfDatabricks/azure/adf.tf
+++ tf/TFProjects/tfDatabricks/azure/adf.tf
@@ -1,12 +1,19 @@
-# module "adfs" {
-#   source = "../../../CommonModules/ADF"
-#   propeties = {
-#     "adf-tfdemo-01-${var.env}" = {
-#       "loc"                 = module.rg.rg-locations["tfdemo-rg-${var.env}"],
-#       "rgname"              = module.rg.rg-names["tfdemo-rg-${var.env}"],
-#       "PublicAccessEnabled" = false,
-#       "ManagedIdentity"     = true,
-#       "tags"                = { "TerraformDeveloper" = "Moein" }
-#     }
-#   }
-# }
+module "adfs" {
+  source = "../../../CommonModules/ADF"
+  propeties = {
+    "adf-tfdb-${var.env}" = {
+      "loc"                 = module.rg.rg-locations["rg-dbrg-${var.env}"],
+      "rgname"              = module.rg.rg-names["rg-dbrg-${var.env}"],
+      "PublicAccessEnabled" = false,
+      "ManagedIdentity"     = true,
+      "tags"                = { "TerraformDeveloper" = "Moein" }
+    }
+  }
+}
+
+# Create a KeyVault Linked Service
+resource "azurerm_data_factory_linked_service_key_vault" "ls-kv-adf" {
+  name            = "ls-kv-${var.env}"
+  data_factory_id = module.adfs.ids["adf-tfdemo-01-${var.env}"]
+  key_vault_id    = module.kv.KVids["kv-tfdb-${var.env}"]
+}

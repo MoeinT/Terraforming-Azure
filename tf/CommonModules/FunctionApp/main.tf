a9f7f9985a834b1e6a4d1e8bf158b38640f301c0
Torabi Mohammadmoein <mohammadmoein.torabi@volvo.com>
2023-02-03 15:39:32 +0100

    added a windows function app

---
+++ tf/CommonModules/FunctionApp/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_windows_function_app" "TestFunctionApp" {
+  for_each = var.properties
+
+  name                        = each.key
+  location                    = each.value.location
+  resource_group_name         = each.value.azurerm_resource_group
+  service_plan_id             = each.value.service_plan_id
+  storage_account_name        = each.value.storage_account_name
+  storage_account_access_key  = each.value.storage_account_access_key
+  functions_extension_version = each.value.functions_extension_version
+  client_certificate_mode     = "Optional"
+  https_only                  = "true"
+  tags                        = contains(keys(each.value), "tags") ? merge(local.DefaultTags, each.value.tags) : local.DefaultTags
+
+  dynamic "identity" {
+    for_each = contains(keys(each.value), "ManagedIdentity") ? each.value.ManagedIdentity == true ? [1] : [] : []
+    content {
+      type = "SystemAssigned"
+    }
+  }
+
+  site_config {
+    always_on = false
+    application_stack {
+      dotnet_version = each.value.dotnet_version
+      }
+  }
+  
+}

ccefd737b05ac3a3eb3f672a5e31e9d16b2efa53
Torabi Mohammadmoein <mohammadmoein.torabi@volvo.com>
2023-02-08 10:54:26 +0100

    added a cosmos DB account

--- tf/TFProjects/tfDatabricks/azure/CosmosDB.tf
+++ tf/TFProjects/tfDatabricks/azure/CosmosDB.tf
@@ -1,13 +1,12 @@
-
-/* module "cosmosdbaccount" {
+module "cosmosdbaccount" {
   source = "../../../CommonModules/CosmosDBAccount"
   properties = {
     "testcosmos-${var.env}" = {
-      "location"             = module.rg.rg-locations["rg-dbrg-${var.env}"],
-      "resource_group_name"  = module.rg.rg-names["rg-dbrg-${var.env}"],
-      "kind"                 = "GlobalDocumentDB",
-      "consistency_level"    = "Session",
-      "capabilities" = ["EnableAggregationPipeline"]
+      "location"            = module.rg.rg-locations["rg-dbrg-${var.env}"],
+      "resource_group_name" = module.rg.rg-names["rg-dbrg-${var.env}"],
+      "kind"                = "GlobalDocumentDB",
+      "consistency_level"   = "Session",
+      "capabilities"        = ["EnableAggregationPipeline"]
     }
   }
-} */
+}

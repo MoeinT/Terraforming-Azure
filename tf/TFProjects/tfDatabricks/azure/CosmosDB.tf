7111dacd48b3c048226b8ad18e96ba9a6520d200
Torabi Mohammadmoein <mohammadmoein.torabi@volvo.com>
2023-02-08 14:33:53 +0100

    added a sql db in cosmos

--- tf/TFProjects/tfDatabricks/azure/CosmosDB.tf
+++ tf/TFProjects/tfDatabricks/azure/CosmosDB.tf
@@ -13,3 +13,13 @@ module "cosmosdbaccount" {
     }
   }
 }
+
+module "cosmosSqlDB" {
+  source = "../../../CommonModules/CosmosDBsqlDB"
+  properties = {
+    "Families-${var.env}" = {
+      "resource_group_name" = module.rg.rg-names["rg-dbrg-${var.env}"],
+      "account_name"        = module.cosmosdbaccount.names["testcosmos-${var.env}"]
+    }
+  }
+}
